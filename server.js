35da14916175e3c7f1dad91489816ef2a9adb881
hassan.ehsani <hassan.ehsani.am@gmail.com>
2025-06-27 16:21:35 +0300

    Initial commit - web chat app

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// server.js
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
@@ -6,21 +7,22 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static('public')); // پوشه برای فایل های فرانت‌اند
+app.use(express.static('public')); // مطمئن شو HTML داخل پوشه public هست
 
 io.on('connection', (socket) => {
   console.log('a user connected');
 
   socket.on('set username', (username) => {
-  socket.username = username;
-  console.log(`${username} joined`);
-  // اطلاع به همه کاربران
-  io.emit('chat message', { username: '⚡ SYSTEM', message: `${username} joined the chat` });
-});
+    socket.username = username;
+    console.log(`${username} joined`);
+    io.emit('chat message', {
+      username: '⚡ SYSTEM',
+      message: `${username} joined the chat`,
+    });
+  });
 
-
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', { username: socket.username, message: msg });
+  socket.on('chat message', (data) => {
+    io.emit('chat message', data);
   });
 
   socket.on('disconnect', () => {
